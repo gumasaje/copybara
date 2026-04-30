a6dabec38488dd8aed1892e3c47ad29e3ff829ca
gumasaje <191728697+gumasaje@users.noreply.github.com>
2026-04-30 17:59:28 +0900

    Initial commit

---
+++ src/main/java/com/gumasaje/algoreview/AlgoreviewApplication.java
@@ -0,0 +1,13 @@
+package com.gumasaje.algoreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgoreviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gumasaje/algoreview/AlgoreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumasaje.algoreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
